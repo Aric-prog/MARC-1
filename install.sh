7a5905e8a72c4359310ead9b9e0919baa9769e63
Jason Christian H <nakamarujc@gmail.com>
2022-01-09 01:27:23 +0700

    Updated install.sh to generate marc1 unique UUID

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Sike, nothing here yet"
+
+# Generate unique UUID for marc1
+# TODO: Move to generateidentity.py (syncs to server)
+# Print uuid as qr code (use a png generator?)
+uuidgen > uuid
+
+echo "Succesfully generated new marc1 UUID!:\n"
+cat uuid
