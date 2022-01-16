6e468f10ac4ba44a15e818fa78a8ca21e80353a2
Aric <hernandoaric@gmail.com>
2022-01-16 20:08:13 +0700

    added ping for when server requests a stream, TODO : do get request on jason's endpoint and get status code from there

--- central/src/socket/streamHandler.js
+++ central/src/socket/streamHandler.js
@@ -1,13 +1,12 @@
+const config = require('../config/config')
 module.exports = function(socket) {
   socket.on('streamRequest', function(token){
     const marciUUID = config.marciUUID
-    // Do logic here where marci sends in their uuid, and also token to jason's stream server
-    
-    // Pings server back when stream attempt failed, 
-    // TODO : figure out how to talk to frontend when stream failed
-    let failedToStream = false;
-    if(failedToStream){
-      socket.emit('streamFailed')
-    }
+    // Do logic here where marci sends in their uuid, and also token to jason's stream server (on the /on_publish endpoint i think)
+    // Pings server back on what happened to stream attempt both failed and not, this will be determine the http response. 
+    console.log('stream request happened')
+    let statusCode = 200;
+
+    socket.emit('streamStatus', statusCode)
   })
 }
