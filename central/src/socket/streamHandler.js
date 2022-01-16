5ec48305f3c4deeed644b67be4f919507f113bd4
Aric <hernandoaric@gmail.com>
2022-01-16 20:59:48 +0700

    comments lol

--- central/src/socket/streamHandler.js
+++ central/src/socket/streamHandler.js
@@ -4,9 +4,13 @@ module.exports = function(socket) {
     const marciUUID = config.marciUUID
     // Do logic here where marci sends in their uuid, and also token to jason's stream server (on the /on_publish endpoint i think)
     // Pings server back on what happened to stream attempt both failed and not, this will be determine the http response. 
+
+    // Do ffmpeg shenanigans (bash) here 
+    
     console.log('stream request happened')
     let statusCode = 200;
 
+    // This stream status echoes back to resolve frontend request
     socket.emit('streamStatus', statusCode)
   })
 }
