a8fd1fd94b8a08c6f7029c8ac34953f44b36149e
Aric <hernandoaric@gmail.com>
2022-01-19 11:08:49 +0700

    added routes for speech api, responds in json

--- movement/api.py
+++ movement/api.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
+from flask import Flask
 
-bp = Blueprint("index", __name__, url_prefix="/")
+app = Flask(__name__)
 
-# TODO : Expose movement routes here, 
+bp = Blueprint("movement", __name__, url_prefix="/move")
+
+# TODO : Expose movement routes here, integrate with bryan
 @bp.route("/")
 def index():
     return ""
 
+if(__name__ == '__main__'):
+    app.register_blueprint(bp)
+    app.run(port=8001, host='localhost')
--- speech/api.py
+++ speech/api.py
@@ -1,20 +1,28 @@
+from itsdangerous import json
 import speech_recognition as sr
 import re
-r = sr.Recognizer()
+from flask import Flask
+from flask import Blueprint
+
+app = Flask(__name__)
 
 # TODO : Initialize flask server here with their endpoints, also find out how to do http request to the other sockets 
+r = sr.Recognizer()
+bp = Blueprint("speech", __name__, url_prefix="/speech")
+
+@bp.route("/listen")
 def startListening():
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         print('Marci is now listening')
         audio = r.listen(source)
-
+        
         try:
             # Separate intent and parameter
             # Get keywords like go, move, set alarm,
             text = r.recognize_google(audio)
             print("Recognized voice input : ", text)
-            detectKeyword(text)
+            return detectKeyword(text)
 
         except:
             print('Marci could not understand the command')
@@ -22,43 +30,59 @@ def startListening():
 def listOfWordExistInString(words, text):
     return set(words).intersection(text.split())
 
+# TODO : replace this with actual move amount
 def move(orientation, amount):
     print(f"Move {orientation} {amount} cm")
 
 def detectKeyword(text):
     text = text.lower()
     text = re.sub('[^a-zA-Z0-9 \n]', '', text)
+
+    response = {}
+
     if(listOfWordExistInString(['note'], text)):
         # Eat words from the word 'note' to the right
         textContent = text.split('note')[1].strip()
-        print("take a note : ", textContent)
+        response['event'] = 'note'
+        response['content'] = textContent
     elif(listOfWordExistInString(['go', 'move'], text)):
-        orientationList = {
-            'forward' : move,
-            'back' : move,
-            'left' : move,
-            'right' : move
-        }
+        orientationList = [
+            'forward',
+            'back',
+            'left',
+            'right' 
+        ]
         # Find parameters such as the amount you go (1, 5) and direction
         moveAmount = re.search(r'\d+', text).group()
         orientation = ""
+         
         for i in orientationList.keys():
             if(i in text):
                 orientation = i
                 break
-        if(orientation != ""):
-            orientationList[orientation](orientation, moveAmount)
-        else:
-            print("No orientation detected, command denied")
+
+        response['event'] = 'move'
+        response['orientation'] = orientation
+        response['moveAmount'] = moveAmount
+
+        if(orientation != ''):
+            return 401
     elif(listOfWordExistInString(['alarm'], text)):
         # PARSE CLOCK :((
-        print(text)
-        amount = re.search(r'\d+', text).group()
-        
+        time = re.search(r'\d+', text).group()
         meridiem = ''
+
         for i in ['am', 'pm']:
             if(i in text):
                 meridiem = i
                 break
-        print('setting alarm at :', amount, meridiem)
 
+        response['event'] = 'alarm'
+        response['meridiem'] = meridiem
+        response['time'] = time
+    print(response)
+    return json.dumps(response, indent = 4)
+
+if __name__ == '__main__':
+    app.register_blueprint(bp)
+    app.run(port=8002, host='localhost')
