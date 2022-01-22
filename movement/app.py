8a0aedd89e060ae4dbcc1b532aad6fc5293afe9b
Aric <hernandoaric@gmail.com>
2022-01-22 21:13:03 +0700

    changes for demo

--- movement/app.py
+++ movement/app.py
@@ -1,24 +1,23 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from flask import Flask
 app = Flask(__name__)
-# import marc0brain
+import marc0brain
 
 bp = Blueprint("movement", __name__, url_prefix="/")
 
 # TODO : Expose movement routes here, integrate with bryan
 
-# movementFunctions = {
-#     'forward' : marc0brain.forward,
-#     'back' : marc0brain.reverse,
-#     'left' : marc0brain.turnLeft,
-#     'right' : marc0brain.turnRight,
-# }
+movementFunctions = {
+    'forward' : marc0brain.forward,
+    'back' : marc0brain.reverse,
+    'left' : marc0brain.turnLeft,
+    'right' : marc0brain.turnRight,
+}
 
 @bp.route("/move", methods=['POST'])
 def determineMovement():
     print('move to', request.form['orientation'])
-    # return marc0brain.generalMove(movementFunctions[request.form['orientation']])
-    return 'succ'
+    return Response(status=marc0brain.generalMove(movementFunctions[request.form['orientation']]))
     
 app.register_blueprint(bp)
 app.run(port=8001, host='localhost')
