1577282cfa860a475f5b07c9a283882c7bc6e55f
Aric <hernandoaric@gmail.com>
2022-01-22 21:07:19 +0700

    completed movement relay

--- central/src/index.js
+++ central/src/index.js
@@ -1,7 +1,7 @@
 // This file should launch on start up
 // Immediately connects to MARC0 backend server, listens to event such as first time setup and video requests
 const http = require('http');
-const request = require()
+const request = require('request')
 const cors = require('cors');
 const express = require('express');
 const io = require('socket.io-client');
@@ -21,11 +21,15 @@ socket.on('connect', function(){
 
 for (const i of ['left', 'right', 'forward', 'back']){
   socket.on('move' + i, function(){
-    request('http://localhost:8001/move' + i, function(req, res){
-      
+    console.log(i)
+    request.post('http://localhost:8001/move', { form : { orientation : i}}, function(req, res){
     })
   })
 }
-
+socket.on('listen', function(){
+  request.get('http://localhost:8002/listen' + i, function(req, res){
+    console.log(res)
+  })
+})
 
 server.listen(8000)
